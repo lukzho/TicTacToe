a036b1a6ea37e20b60e5fc7dcd863ac2b79d9016
Luk <wi23b143@technikum-wien.at>
2024-07-02 12:50:54 +0200

    us1_testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.start();
+        System.out.println("hello");
     }
 }
