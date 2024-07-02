fedd3676da94c76800cd2d18f08224d88d445788
Luk <wi23b143@technikum-wien.at>
2024-07-02 14:41:10 +0200

    us2 created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.start();
-
+        while (!ticTacToe.isGameOver()) {
+            ticTacToe.displayBoard();
+        }
     }
 }
