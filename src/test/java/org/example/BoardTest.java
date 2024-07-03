73809736a433734145acc1400184bd2285adc48a
Luk <wi23b143@technikum-wien.at>
2024-07-03 12:35:17 +0200

    Test Method implemented for the method hasEnded

--- src/test/java/org/example/BoardTest.java
+++ src/test/java/org/example/BoardTest.java
@@ -36,6 +36,42 @@ class BoardTest {
         board.place(1,2,'X');
         assertEquals('X',board.cells[1][2]);
     }
+    @Test
+    void hasEnded(){
+        board.cells[0][0]='X';
+        board.cells[1][1]='X';
+        board.cells[2][2]='X';
+        assertTrue(board.hasEnded());
+        board.cells[0][0]='X';
+        board.cells[0][1]='X';
+        board.cells[0][2]='X';
+        assertTrue(board.hasEnded());
+        board.cells[1][0]='X';
+        board.cells[1][1]='X';
+        board.cells[1][2]='X';
+        assertTrue(board.hasEnded());
+        board.cells[2][0]='X';
+        board.cells[2][1]='X';
+        board.cells[2][2]='X';
+        assertTrue(board.hasEnded());
+        board.cells[0][0]='X';
+        board.cells[1][0]='X';
+        board.cells[2][0]='X';
+        assertTrue(board.hasEnded());
+        board.cells[0][1]='X';
+        board.cells[1][1]='X';
+        board.cells[2][1]='X';
+        assertTrue(board.hasEnded());
+        board.cells[0][2]='X';
+        board.cells[1][2]='X';
+        board.cells[2][2]='X';
+        assertTrue(board.hasEnded());
+        board.cells[0][2]='X';
+        board.cells[1][1]='X';
+        board.cells[2][0]='X';
+        assertTrue(board.hasEnded());
+    }
+
 
 }
 /*
