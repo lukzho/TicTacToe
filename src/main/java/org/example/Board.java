ce2291c064b1e14698a565fe317c1406fb95635d
Tareq Elkhadra <tarekalkhdramal.com>
2024-07-06 16:08:15 +0200

    Game Ready

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,44 +1,88 @@
 package org.example;
 
 public class Board {
+    public char[][] cells;
 
-    public char [][] cells;
-
-    public Board(){
+    public Board() {
         cells = new char[3][3];
-        for(int i = 0; i < cells.length;i++){
-            for(int j = 0; j < cells[i].length;j++){
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
                 cells[i][j] = ' ';
             }
-            System.out.print("\n");
         }
-    };
+    }
 
-    public boolean isCellEmpty(int x, int y){
-        if(x>=0 && x <3 && y >=0 && y<3){
-            if (cells[x][y]==' ')
-                return true;
-            else
-                return false;
-
+    public boolean isCellEmpty(int x, int y) {
+        if (x >= 0 && x < 3 && y >= 0 && y < 3) {
+            return cells[x][y] == ' ';
         }
         return false;
-
     }
-    public void place(int x, int y, char marker){
-        if(isCellEmpty(x,y)){
+
+    public void place(int x, int y, char marker) {
+        if (isCellEmpty(x, y)) {
             cells[x][y] = marker;
         }
     }
-    public void print() {
-    }
-    public String getBoardForPrint() {
-        return null;}
 
-    public char getCell(int r, int c){
-        return ' ';}
-    public boolean isFull(){
-        return true;}
-    public void clear() {
+    public void print() {
+        System.out.println("----------------");
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                System.out.print("| " + cells[i][j] + " |");
+            }
+            System.out.println();
+            System.out.println("----------------");
+        }
     }
+
+
+    public char getCell(int r, int c) {
+        if (r >= 0 && r < 3 && c >= 0 && c < 3) {
+            return cells[r][c];
+        }
+        return ' ';
+    }
+
+    public boolean isFull() {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                if (cells[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void clear() {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = ' ';
+            }
+        }
+    }
+    public boolean hasWinner() {
+        // Check the diagonal from left
+        if (cells[0][0] != ' ' && cells[0][0] == cells[1][1] && cells[1][1] == cells[2][2]) {
+            return true;
+        }
+        // Check the diagonal from right
+        if (cells[0][2] != ' ' && cells[0][2] == cells[1][1] && cells[1][1] == cells[2][0]) {
+            return true;
+        }
+        // Check rows and columns
+        for (int i = 0; i < 3; i++) {
+            // Check rows
+            if (cells[i][0] != ' ' && cells[i][0] == cells[i][1] && cells[i][1] == cells[i][2]) {
+                return true;
+            }
+            // Check columns
+            if (cells[0][i] != ' ' && cells[0][i] == cells[1][i] && cells[1][i] == cells[2][i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -12,37 +12,55 @@ public class TicTacToe {
         board = new Board();
         player1 = new Player('X');
         player2 = new Player('O');
-        currentPlayer = player1;
+        currentPlayer = player2;
     }
 
     public void start(){
         Scanner sc = new Scanner(System.in);
-        int row;
-        int col;
-
         while(true) {
-            System.out.printf("Current Player: %c\n",currentPlayer.getMarker());
-            do {
-                System.out.print("row (0-2): ");
-                row = sc.nextInt();
-                if(row>2 || row<0){
-                    System.out.println("Please enter a number between 0 and 2");
-                }
-            }while(row>2 || row<0);
-            do {
-                System.out.print("column (0-2): ");
-                col = sc.nextInt();
-                if(col>2 || col<0){
-                    System.out.println("Please enter a number between 0 and 2");
-                }
-            }while(col>2 || col<0);
-
-            board.place(row, col, currentPlayer.getMarker());
-
-            currentPlayer = currentPlayer==player1 ? player2 : player1;
+            board.print();
+            if(board.hasWinner()){
+                System.out.println("The Player:" + currentPlayer.getMarker() + " has won the Game");
+                board.clear();
+                continue;
+            }
+            if(board.isFull()){
+                System.out.println("Its Draw");
+                board.clear();
+                continue;
+            }
+            switchPlayer();
+            playTurn(sc);
         }
-        //sc.close();
+        // sc.close();
     }
 
+    private void playTurn(Scanner sc) {
+        while(true) {
+            System.out.printf("Current Player: %c\n", currentPlayer.getMarker());
+            int row = getValidInput(sc, "row");
+            int col = getValidInput(sc, "column");
+            if(board.isCellEmpty(row,col)){
+                board.place(row, col, currentPlayer.getMarker());
+                break;
+            }
+            System.out.println("Invaild Poistion");
+        }
+    }
 
+    private int getValidInput(Scanner sc, String type) {
+        int value;
+        do {
+            System.out.printf("%s (0-2): ", type);
+            value = sc.nextInt();
+            if (value > 2 || value < 0) {
+                System.out.println("Please enter a number between 0 and 2");
+            }
+        } while (value > 2 || value < 0);
+        return value;
+    }
+
+    private void switchPlayer() {
+        currentPlayer = (currentPlayer == player1) ? player2 : player1;
+    }
 }
